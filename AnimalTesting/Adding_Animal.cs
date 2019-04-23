1ee729c4860d18e80be4f7dee3d1c75f3879fb45
Elise Luitgaarden <elise.luitgaarden@gmail.com>
2019-04-23 10:42:49 +0200

    UnitTesting

--- AnimalTesting/Adding_Animal.cs
+++ AnimalTesting/Adding_Animal.cs
@@ -5,7 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace AnimalTesting
 {
     [TestClass]
-    public class AddingAnimal
+    public class Adding_Animal
     {
         Administration admin = new Administration();
         Animal animala = new Cat(13, new SimpleDate(9, 8, 1998), "Lerry", "");
@@ -34,46 +34,33 @@ namespace AnimalTesting
         [TestMethod]
         public void Test_Animal_Will_Change_Number()
         {
-            int number = 13;
+            Add_Animal_To_AllAnimal_List();
 
-            Animal animala = new Cat(number, new SimpleDate(9, 8, 1995), "Harry", "");
-            admin.Add(animala);
-            
-            Animal animalb = new Dog(number, new SimpleDate(9, 8, 2017), "Louise", new SimpleDate(7, 8, 2018));
-            while (!admin.Add(animalb))
+            int number = 16;
+            Animal animale = new Dog(number, new SimpleDate(9, 8, 2017), "Louise", new SimpleDate(7, 8, 2018));
+            while (!admin.Add(animale))
             {
                 number++;
-                animalb = new Dog(number, new SimpleDate(9, 8, 2017), "Louise", new SimpleDate(7, 8, 2018));
+                animale = new Dog(number, new SimpleDate(9, 8, 2017), "Louise", new SimpleDate(7, 8, 2018));
             }
 
-            Assert.AreEqual(14, animalb.ChipRegistrationNumber);
+            Assert.AreEqual(17, animale.ChipRegistrationNumber);
         }
 
         [TestMethod]
         public void Test_Add_And_Find_Animal()
         {
-            int number = 13;
-            Animal animal = new Cat(number, new SimpleDate(9, 8, 1995), "Harry", "");
-            Administration admin = new Administration();
+            Add_Animal_To_AllAnimal_List();
 
-            admin.Add(animal);      
+            admin.Add(animala);      
             
-            Assert.AreEqual(animal, admin.FindAnimal(13));
+            Assert.AreEqual(animala, admin.FindAnimal(13));
         }
 
         [TestMethod]
         public void Test_All_Added_Animals_Are_Animals()
         {
-            Administration admin = new Administration();
-            Animal animala = new Cat(13, new SimpleDate(9, 8, 1998), "Lerry", "");
-            Animal animalb = new Dog(14, new SimpleDate(9, 8, 1996), "Harry", new SimpleDate(8, 9, 2019));
-            Animal animalc = new Dog(15, new SimpleDate(9, 8, 2017), "Sam", new SimpleDate(5, 7, 2018));
-            Animal animald = new Cat(16, new SimpleDate(9, 8, 2005), "Kit", "Bites");
-
-            admin.Add(animala);
-            admin.Add(animalb);
-            admin.Add(animalc);
-            admin.Add(animald);
+            Add_Animal_To_AllAnimal_List();
 
             CollectionAssert.AllItemsAreInstancesOfType(admin.AllAnimals, typeof(Animal));
         }
@@ -81,10 +68,7 @@ namespace AnimalTesting
         [TestMethod]
         public void Test_Will_Fail_To_Add_Second_Animal()
         {
-            Animal animal = new Cat(13, new SimpleDate(9, 6, 1995), "harry", "");
-
-            Administration admin = new Administration();
-            admin.Add(animal);
+            Add_Animal_To_AllAnimal_List();
 
             Animal animalTwo = new Dog(13, new SimpleDate(9, 6, 1995), "HarryTwo", new SimpleDate(9, 6, 1995));
             bool b = admin.Add(animalTwo);
--- AnimalTesting/Reserving_And_Selling_Animals.cs
+++ AnimalTesting/Reserving_And_Selling_Animals.cs
@@ -35,11 +35,39 @@ namespace AnimalTesting
         }
 
         [TestMethod]
-        public void Test_Reserving_Animal()
+        public void Test_Animal_Is_Not_Reserved()
         {
             Add_Animal_To_AllAnimal_List();
 
             Assert.IsFalse(animala.IsReserved);
+        }
+
+        [TestMethod]
+        public void Test_Animal_Is_Reserved()
+        {
+            Add_Animal_To_AllAnimal_List();
+            animala.IsReserved = true;
+
+            Assert.IsTrue(animala.IsReserved);
+        }
+
+        [TestMethod]
+        public void Test_Sell_And_Remove_Animal()
+        {
+            Add_Animal_To_AllAnimal_List();
+
+            admin.RemoveAnimal(animalc.ChipRegistrationNumber);
+
+            CollectionAssert.DoesNotContain(admin.AllAnimals, animalc);
+            Assert.AreEqual(350, animalc.Price);
+        }
+
+        [TestMethod]
+        public void Test_Remove_Animal()
+        { 
+            admin.RemoveAnimal(animala.ChipRegistrationNumber);
+
+            Assert.AreEqual(null, admin.FindAnimal(13));
         }
     }
 }
